236c4d173be655dd1cb19e7e707bcdefdf68bc80
hernandosierra <hdsierrao@unal.edu.co>
2022-04-18 11:30:17 -0500

    Primera entrega

--- gummies.js
+++ gummies.js
@@ -40,6 +40,19 @@ const division = (a, b) => a / b
 
 const interesProducto = a => a * 0.02
 
+//Función de las deducciones de los empleados
+
+function deduccionesEmpleados (a){
+    if (a<2500){
+        let resultado = a * 0.04
+        return resultado
+    }
+    else if(a>=2500){
+        let resultado = a * 0.06
+        return resultado
+    }
+}
+
 //Inventario de productos
 
 const producto1 = new Productos ('Dulce de leche', 100)
@@ -61,8 +74,8 @@ const inventarioServicios = [servicio1, servicio2, servicio3]
 
 //Inventario de empleados
 
-const empleado1 = new Empleados ('Juan Álvarez', 1200)
-const empleado2 = new Empleados ('Andrés Rodriguez', 2350)
+const empleado1 = new Empleados ('Juan Alvarez', 1200)
+const empleado2 = new Empleados ('Andres Rodriguez', 2350)
 const empleado3 = new Empleados ('Leonardo Camacho', 1500)
 const empleado4 = new Empleados ('Zayra Pulido', 3800)
 const empleado5 = new Empleados ('Nora Astaiza', 2600)
@@ -86,6 +99,8 @@ if (usuario == 1){
 
     console.log('Estimado cliente, siempre es un placer atenderte')
     let seleccionCliente = prompt('¿Deseas averiguar sobre productos o servicios?').toLowerCase()
+
+    // interaccion con los productos de la empresa
     if (seleccionCliente == 'productos'){
         let seleccionClienteProductos = prompt('¿Buscar, comprar o pagar?').toLowerCase()
         switch(seleccionClienteProductos){
@@ -162,6 +177,7 @@ if (usuario == 1){
 
         }
     }
+    //Interaccion con los servicios de la empresa
     else if (seleccionCliente == 'servicios'){
         let seleccionClienteServicios = prompt('¿Buscar, comprar o pagar?').toLowerCase()
         switch(seleccionClienteServicios){
@@ -256,65 +272,165 @@ if (usuario == 1){
 
 else if (usuario == 2){
 
-    let seleccionPersonal = prompt('Opciones para empleados: inventario de productos; listado de empleados; pagos').toLocaleLowerCase()
+    let seleccionPersonal = prompt('Opciones para empleados: inventario de productos; inventario de servicios; listado de empleados; pagos').toLocaleLowerCase()
 
     switch(seleccionPersonal){
-        case 'inventario':
-            console.log('si')
-        case 'listado':
-            console.log('si')
+        case 'inventario de productos':
+            console.log('El inventario de productos es:')
+            console.log(inventarioProductos)
+            console.log('En su inventario hay la siguiente cantidad de productos: ')
+            console.log(inventarioProductos.length)
+            let seleccionInventarioP = prompt('¿Desea eliminar o añadir un producto?').toLowerCase()
+            if(seleccionInventarioP == 'eliminar'){
+                eliminarProducto = prompt('¿Qué producto desea eliminar?')
+                let resultadoProductoEliminar = inventarioProductos.find(elementoResultanteP => elementoResultanteP.nombre === eliminarProducto)
+                let indiceDelProducto = inventarioProductos.indexOf(resultadoProductoEliminar)
+                let productoEliminado = inventarioProductos.splice(indiceDelProducto, 1)
+                console.log('El inventario actualizado de sus productos es: ')
+                console.log(inventarioProductos)
+                let eliminarMas = prompt('¿Desea eliminar otros productos?')
+                    while(eliminarMas == 'si'){
+                        eliminarProducto = prompt('¿Qué producto desea eliminar?')
+                        let resultadoProductoEliminar = inventarioProductos.find(elementoResultanteP => elementoResultanteP.nombre === eliminarProducto)
+                        let indiceDelProducto = inventarioProductos.indexOf(resultadoProductoEliminar)
+                        let productoEliminado = inventarioProductos.splice(indiceDelProducto, 1)
+                        console.log('El inventario actualizado de sus productos es: ')
+                        console.log(inventarioProductos)
+                        let eliminarMas = prompt('¿Desea eliminar otros productos?')
+                        if (eliminarMas != 'si'){
+                            break
+                        } 
+                    }
+                    console.log('¡Disfrute de la página!')
+            }
+            else if(seleccionInventarioP == 'añadir'){
+                const nuevoProducto = new Productos (prompt('Ingrese el nombre del producto'), parseInt(prompt('Ingrese el precio del producto')))
+                let productoAñadido = inventarioProductos.push(nuevoProducto)
+                console.log('El inventario actualizado de sus productos es: ')
+                console.log(inventarioProductos)
+                let añadirMas = prompt('¿Desea añadir más productos?')
+                while(añadirMas == 'si'){
+                    const nuevoProducto = new Productos (prompt('Ingrese el nombre del producto'), parseInt(prompt('Ingrese el precio del producto')))
+                    let productoAñadido = inventarioProductos.push(nuevoProducto)
+                    console.log('El inventario actualizado de sus productos es: ')
+                    console.log(inventarioProductos)
+                    let añadirMas = prompt('¿Desea añadir más productos?')
+                    if (añadirMas != 'si'){
+                        break
+                    }  
+                }
+            }
+            console.log('¡Disfrute de la página!')            
+            break
+        case 'inventario de servicios':
+            console.log('El inventario de servicios es:')
+            console.log(inventarioServicios)
+            let seleccionInventarioS = prompt('¿Desea eliminar o añadir un servicio?').toLowerCase()
+            if(seleccionInventarioS == 'eliminar'){
+                eliminarServicio = prompt('¿Qué servicio desea eliminar?')
+                let resultadoServicioEliminar = inventarioServicios.find(elementoResultanteS => elementoResultanteS.nombre === eliminarServicio)
+                let indiceDelServicio = inventarioServicios.indexOf(resultadoServicioEliminar)
+                let ServicioEliminado = inventarioServicios.splice(indiceDelServicio, 1)
+                console.log('El inventario actualizado de sus servicios es: ')
+                console.log(inventarioServicios)
+                let eliminarMas = prompt('¿Desea eliminar otros servicios?')
+                    while(eliminarMas == 'si'){
+                        eliminarServicio = prompt('¿Qué servicio desea eliminar?')
+                        let resultadoServicioEliminar = inventarioServicios.find(elementoResultanteS => elementoResultanteS.nombre === eliminarServicio)
+                        let indiceDelServicio = inventarioServicios.indexOf(resultadoServicioEliminar)
+                        let servicioEliminado = inventarioServicios.splice(indiceDelServicio, 1)
+                        console.log('El inventario actualizado de sus servicios es: ')
+                        console.log(inventarioServicios)
+                        let eliminarMas = prompt('¿Desea eliminar otros servicios?')
+                        if (eliminarMas != 'si'){
+                            break
+                        } 
+                    }
+                    console.log('¡Disfrute de la página!')
+            }
+            else if(seleccionInventarioS == 'añadir'){
+                const nuevoServicio = new Servicios (prompt('Ingrese el nombre del servicio'), parseInt(prompt('Ingrese el precio del servicio')))
+                let servicioAñadido = inventarioServicios.push(nuevoServicio)
+                console.log('El inventario actualizado de sus servicios es: ')
+                console.log(inventarioServicios)
+                let añadirMas = prompt('¿Desea añadir más servicios?')
+                while(añadirMas == 'si'){
+                    const nuevoServicio = new Servicios (prompt('Ingrese el nombre del servicio'), parseInt(prompt('Ingrese el precio del servicio')))
+                    let servicioAñadido = inventarioServicios.push(nuevoServicio)
+                    console.log('El inventario actualizado de sus servicios es: ')
+                    console.log(inventarioServicios)
+                    let añadirMas = prompt('¿Desea añadir más servicios?')
+                    if (añadirMas != 'si'){
+                        break
+                    }  
+                }
+            }
+            console.log('¡Disfrute de la página!')
+            break
+        case 'listado de empleados':
+            console.log('La nómina de empleados es:')
+            console.log(inventarioEmpleados)
+            let seleccionInventarioE = prompt('Se va a despedir o a contratar a un empleado')
+            if (seleccionInventarioE == 'despedir'){
+                despedirEmpleado = prompt('¿Cuál empleado se despedirá?')
+                let resultadoDespedirEmpleado = inventarioEmpleados.find(elementoResultanteE => elementoResultanteE.nombre === despedirEmpleado)
+                let indiceDelEmpleado = inventarioEmpleados.indexOf(resultadoDespedirEmpleado)
+                let empleadoDespedido= inventarioEmpleados.splice(indiceDelEmpleado, 1)
+                console.log('El empleado despedido es: ' +resultadoDespedirEmpleado.nombre)
+                console.log('La nómina actualizada es: ')
+                console.log(inventarioEmpleados)
+                let eliminarMas = prompt('¿Se ha despedido a otros empleados?')
+                    while(eliminarMas == 'si'){
+                        despedirEmpleado = prompt('¿Cuál empleado se despedirá?')
+                        let resultadoDespedirEmpleado = inventarioEmpleados.find(elementoResultanteE => elementoResultanteE.nombre === despedirEmpleado)
+                        let indiceDelEmpleado = inventarioEmpleados.indexOf(resultadoDespedirEmpleado)
+                        let empleadoDespedido= inventarioEmpleados.splice(indiceDelEmpleado, 1)
+                        console.log('El empleado despedido es: ' +resultadoDespedirEmpleado.nombre)
+                        console.log('La nómina actualizada es: ')
+                        console.log(inventarioEmpleados)
+                        let eliminarMas = prompt('¿Se ha despedido a otros empleados?')
+                        if (eliminarMas != 'si'){
+                            break
+                        } 
+                    }
+                    console.log('¡Disfrute de la página!') 
+            }
+            else if (seleccionInventarioE == 'contratar'){
+                const nuevoEmpleado = new Empleados (prompt('Ingrese el nombre del empleado'), parseInt(prompt('Ingrese el sueldo del empleado')))
+                let empleadoContratado = inventarioEmpleados.push(nuevoEmpleado)
+                console.log('La nómina actualizada es: ')
+                console.log(inventarioEmpleados)
+                let añadirMas = prompt('¿Se ha contratado a otros empleados?')
+                while(añadirMas == 'si'){
+                    const nuevoEmpleado = new Empleados (prompt('Ingrese el nombre del empleado'), parseInt(prompt('Ingrese el sueldo del empleado')))
+                    let empleadoContratado = inventarioEmpleados.push(nuevoEmpleado)
+                    console.log('La nómina actualizada es: ')
+                    console.log(inventarioEmpleados)
+                    let añadirMas = prompt('¿Se ha contratado a otros empleados?')
+                    if (añadirMas != 'si'){
+                        break
+                    }  
+                }
+            } 
+            console.log('¡Disfrute de la página!')
+            break
         case 'pagos':
-            console.log('si')
+            let nombreEmpleado = prompt('Estimado empleado ¿Cuál es su nombre?')
+            let resultadoNombreEmpleado = inventarioEmpleados.find(elementoResultanteE => elementoResultanteE.nombre === nombreEmpleado)
+            if(inventarioEmpleados.includes(resultadoNombreEmpleado)){
+                console.log('Su sueldo base es $' +resultadoNombreEmpleado.sueldo)
+                let deduccionEmpleadoPago = deduccionesEmpleados(resultadoNombreEmpleado.sueldo)
+                console.log('Sus deducciones equivalen a $' +deduccionEmpleadoPago) 
+                let pagoEmpleado = resta(resultadoNombreEmpleado.sueldo, deduccionEmpleadoPago)
+                console.log('Se ha debitado a su cuenta $' +pagoEmpleado)
+            }
+            else {
+                console.log('Usted no hace parte del personal')
+                }
+            console.log('¡Disfrute de la página!')
+            break
         default:
             console.log('¡Disfrute de la página!')
             break
     }
-
-    //Desea ver el inventario de productos
-    //Desea modificar el inventario
-    //Añadir productos
-    //Eliminar productos
-
-    //Desea ver el listado de empleados
-    //Desea modificar el listado
-    //Buscar un empleado en el listado
-
-    //Desea recibir su pago
-
 }
-
-//Cierre del simulador
-
-else {
-    console.log('¡Disfrute de la página!')
-}
-
-// el interés se hace con un for
-
-//for (meses = 0; meses <=mesesdeuda; meses++)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
